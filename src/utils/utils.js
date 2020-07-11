ff67aaa887850b6872bbab1f429e81a2777a0340
Tejiri Matthew <39820795+tejiri4@users.noreply.github.com>
2020-07-11 16:08:38 +0000

    Ft interview (#1)
    * answered section 1 question & section 2.1
    
    * answered section 2, q1 - q5, skipped q3b
    
    * image upload

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,5 +1,5 @@
 
-export const pathGet = (arr1, query) => {
+export const pathGet = (data, query) => {
   // TASK 1: 
   // Write a function that searches through the input array / object
   // and returns the appropriate string path leading to the input query, if found
@@ -22,4 +22,28 @@ export const pathGet = (arr1, query) => {
   // `pathGet(a, 'James')` = "a.user.name.firstName"
 
   // ============== CODE GOES BELOW THIS LINE :) ==============
+
+    const searchObject = (data, path='') => {
+        for(const value in data) {
+          if (typeof data[value] === 'string') {
+              if (!data[value].toLowerCase().includes(query.toLowerCase())) {continue};
+              return `${path}.${value}`;
+          }
+          const search = searchObject(data[value], query, `${path}.${value}`);
+          if (search) {
+            return search 
+          }
+        }
+    }
+    return searchObject(data, query, 'data');
+}
+
+
+export const validateField = (fields) => {
+    for (const field in fields) {
+        if (fields[field] === '') {
+            return false;
+        }
+    }
+    return true;
 }
